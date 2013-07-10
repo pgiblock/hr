9c1ea59946f4d04fcd0acbf86e49129085fd1ca3
Paul Giblock <p@pgiblock.net>
2013-07-10 17:01:28 -0400

    Resurrect this old code
    Change to my latest code style
    Add some comments

--- hr.c
+++ hr.c
@@ -4,62 +4,68 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 
-#define HR_SYMBOL "HR_SYMBOL"
+#define ENV_SYMBOL "HR_SYMBOL"
+#define DEFAULT_SYMBOL "="
 
 static void
-print_usage (FILE *f, char *exe) {
-	//TODO: New usage: hr [-s<line>] text to center
-	fprintf(f, "Usage: %s [symbol]\n", exe);
+print_usage (FILE *f, char *exe)
+{
+  //TODO: New usage: hr [-s<line>] text to center
+  fprintf(f, "Usage: %s [symbol]\n", exe);
 }
 
 int
-main (int argc, char **argv) {
-	struct winsize ws;
-	int len, i;
+main (int argc, char **argv)
+{
+  struct winsize ws;
+  int len, i;
 
-	char *str, *tmp;
-	switch (argc) {
-		case 1:
-			tmp = getenv(HR_SYMBOL);
-			if (tmp) {
-				str = tmp;
-				len = strlen(tmp);
-			} else {
-				str = "=";
-				len = 1;
-			}
-			break;
+  char *sym, *tmp;
+  switch (argc) {
+    case 1:
+      // No arguments: use symbol from env-var, if available
+      tmp = getenv(ENV_SYMBOL);
+      if (tmp) {
+        sym = tmp;
+        len = strlen(sym);
+      } else {
+        // Default symbol
+        sym = DEFAULT_SYMBOL;
+        len = 1;
+      }
+      break;
 
-		case 2:
-			if (!strcmp(argv[1], "--help")) {
-				print_usage(stdout, argv[0]);
-				printf("Displays a horizontal rule for finding your place in terminal history.\n");
-				exit(0);
-			}
-			str = argv[1];
-			len = strlen(str);
-			break;
+    case 2:
+      // Argument specified: either a flag or a symbol
+      if (strcmp(argv[1], "--help") == 0) {
+        print_usage(stdout, argv[0]);
+        printf("Displays a horizontal rule for finding your place in terminal history.\n");
+        exit(0);
+      }
+      sym = argv[1];
+      len = strlen(sym);
+      break;
 
-		default:
-			// TODO: Concatenate all arguments with spaces (and print an extra space between repetitions)
-			fprintf(stderr, "Error: unexpected number of arguments\n");
-			print_usage(stderr, argv[0]);
-			exit(1);
-	}
-			
-	ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
+    default:
+      // TODO: Concatenate all arguments with spaces (and print an extra space between repetitions)
+      fprintf(stderr, "Error: unexpected number of arguments\n");
+      print_usage(stderr, argv[0]);
+      exit(1);
+  }
 
-	// Up one line
-	fputs("\033[A", stdout);
+  // Fetch the window size of the terminal
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
 
-	// Print horizontal rule
-	// TODO: Handle cols < len
-	for (i = 0; i <= ws.ws_col-len; i += len) {
-		fputs(str, stdout);
-	}
-	// The remainder
-	fwrite(str, 1, ws.ws_col-i, stdout);
+  // Up one line
+  fputs("\033[A", stdout);
 
-	return 0;
+  // Print horizontal rule
+  for (i = 0; i <= ws.ws_col-len; i += len) {
+    fputs(sym, stdout);
+  }
+  // The remainder
+  fwrite(sym, 1, ws.ws_col-i, stdout);
+
+  return 0;
 }
 
